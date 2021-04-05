e4cc0fc7955ff65f3e02ff25658576a8792dfff3
nadiafarhanii <56537186+nadiafarhanii@users.noreply.github.com>
2021-04-05 12:47:08 +0700

    index.php
    mengubah di bagian judul

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 <html lang="en">
 <head>
     <title>Data Pengiriman Barang</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <script src="jquery/jquery-3.4.1.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
 
 </head>
 <body>
